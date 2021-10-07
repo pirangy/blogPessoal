88f7f9b7b7ff7db5e7963112ff90a1cca48cdf20
pirangy <pirangy.ariel@gmail.com>
2021-10-07 10:13:30 -0300

    Implementação de PUT e DELETE de Postagens

--- src/app/service/postagem.service.ts
+++ src/app/service/postagem.service.ts
@@ -2,6 +2,7 @@ import { HttpClient, HttpHeaders } from '@angular/common/http';
 import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
 import { environment } from 'src/environments/environment.prod';
+import { PostagemEditComponent } from '../edit/postagem-edit/postagem-edit.component';
 import { Postagem } from '../model/Postagem';
 
 @Injectable({
@@ -19,8 +20,21 @@ export class PostagemService {
     return this.http.get<Postagem[]>('https://blogpessoalback.herokuapp.com/postagens', this.token)
   }
 
+  getByIdPostagem(id: number): Observable<Postagem>{
+    return this.http.get<Postagem>(`https://blogpessoalback.herokuapp.com/postagens/${id}`, this.token)
+  }
+
   postPostagem(postagem: Postagem): Observable<Postagem>{
     return this.http.post<Postagem>('https://blogpessoalback.herokuapp.com/postagens', postagem ,this.token)
+  }
+
+  putPostagem(postagem: Postagem): Observable<Postagem>{
+    return this.http.put<Postagem>('https://blogpessoalback.herokuapp.com/postagens', postagem, this.token)
+
+  }
+
+  deletePostagem(id: number): Observable<Postagem>{
+    return this.http.delete<Postagem>(`https://blogpessoalback.herokuapp.com/postagens/${id}`, this.token)
   }
 
 
